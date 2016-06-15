560adee38a6d7560d64e4df97f4a3b2b559909ef
Mauricio Montalvo <mauricio@wizeservices.com>
2016-06-14 20:36:50 -0500

    Temp

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var test = require('./routes/test');
+var mongo = require('mongodb');  
+require('./model/db');
 
 var app = express();
 
--- routes/test.js
+++ routes/test.js
@@ -1,11 +1,71 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Request = require('../model/request');
+var Q = require('q');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log('conection');
-  console.log(req.query);
-  res.send({ message: 'Moonki Webapp' });
+router.get('/list', function(req, res) {
+    var promise = getList();
+
+    promise.then(
+        function(collection){
+            res.json(collection);
+        }, 
+        function(){
+          res.json({'message': 'Error'});
+        }
+    );	
 });
+
+function getList(){
+    
+  var deferred = Q.defer();
+  var query = Request.find({});
+
+  query.exec(function (err, collection){
+    if(err) {
+      deferred.reject();
+    }
+    else {
+      deferred.resolve(collection);
+    }
+  });
+
+    return deferred.promise;
+}
+
+
+function save(object){
+    
+    var deferred = Q.defer();
+    var news = new Request({data: JSON.stringify(object)});
+    
+    news.save(function (err) 
+    {
+        if (err){
+            deferred.reject();
+        }
+        else{
+            deferred.resolve(news);
+        } 
+    });
+    
+    return deferred.promise;
+}
+
+router.get('/', function(req, res) { 
+    var promise = save(req.query);
+    
+    promise.then(
+        function(shortUrl){
+          res.json(shortUrl);
+        },
+        function(){
+            res.json({'message': 'Error'});
+        }
+    );	
+});
+
 
 module.exports = router;
