b2f1ee730bd9f57020e5c351f0c8529fa3b184de
Mauricio Montalvo <mauricio@wizeservices.com>
2016-07-01 19:30:43 -0500

    Changes landing page message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Moonki Webapp' });
+  res.render('index', { title: 'Awesome Moonki Webapp' });
 });
 
 router.get('/health', function (req, res) {
